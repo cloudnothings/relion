f82c815b9eed02b76167790d86e67138e3121d86
Carlos Rangel <carlos@rangel.us>
2022-08-29 21:32:41 -0700

    Updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { GetStaticProps } from "next";
 import Layout from "../components/Layout";
 import Post, { PostProps } from "../components/Post";
 import prisma from "../lib/prisma";
+import { useSession } from "next-auth/react";
 export const getStaticProps: GetStaticProps = async () => {
   const feed = await prisma.post.findMany({
     where: { published: true },
@@ -23,34 +24,50 @@ type Props = {
 };
 
 const Blog: React.FC<Props> = (props) => {
-  return (
+  const { data: session, status } = useSession();
+  let view = (
     <Layout>
-      <div className="page">
-        <h1>Public Feed</h1>
-        <main>
-          {props.feed.map((post) => (
-            <div key={post.id} className="post">
-              <Post post={post} />
-            </div>
-          ))}
-        </main>
-      </div>
-      <style jsx>{`
-        .post {
-          background: white;
-          transition: box-shadow 0.1s ease-in;
-        }
-
-        .post:hover {
-          box-shadow: 1px 1px 3px #aaa;
-        }
-
-        .post + .post {
-          margin-top: 2rem;
-        }
-      `}</style>
+      <></>
     </Layout>
   );
+
+  if (status === "loading")
+    view = (
+      <Layout>
+        <>Loading</>
+      </Layout>
+    );
+
+  if (session)
+    view = (
+      <Layout>
+        <div className="page">
+          <h1>Public Feed</h1>
+          <main>
+            {props.feed.map((post) => (
+              <div key={post.id} className="post">
+                <Post post={post} />
+              </div>
+            ))}
+          </main>
+        </div>
+        <style jsx>{`
+          .post {
+            background: white;
+            transition: box-shadow 0.1s ease-in;
+          }
+
+          .post:hover {
+            box-shadow: 1px 1px 3px #aaa;
+          }
+
+          .post + .post {
+            margin-top: 2rem;
+          }
+        `}</style>
+      </Layout>
+    );
+  return <>{view}</>;
 };
 
 export default Blog;
---
+++ pages/login.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type Props = {};
+
+const Login = (props: Props) => {
+  return (
+    <>
+      <div>Login</div>
+      <button>Sign in</button>
+    </>
+  );
+};
+
+export default Login;
